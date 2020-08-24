aafaf91d21e80e3fae46865699219272142ef227
rakutek <rakutek12@gmail.com>
2020-08-25 00:11:20 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Clock from "./components/Clock";
+
 function App() {
   return (
     <div className="App">
@@ -10,14 +12,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Clock date={new Date()} />
+
       </header>
     </div>
   );
